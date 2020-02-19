49c97745130747f9d58ba4cd0efb1c2f98f60a34
codemma <emma.tysk@gmail.com>
2020-02-19 15:30:02 +0100

    added push to Firebase to pants folder

--- screens/MyPantScreen.js
+++ screens/MyPantScreen.js
@@ -1,13 +1,73 @@
 import * as firebase from "firebase";
-import React, { useState } from "react";
-import { Button, StyleSheet, Text, View, Alert } from "react-native";
+import React, { useState, Component } from "react";
+import {
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+  Text,
+  TextInput
+} from "react-native";
 
 export default function MyPant() {
+  const [cansCount, setCanAmount] = useState(0);
+  const user = firebase.auth().currentUser.uid;
+
+  var pantsRef = firebase.database().ref("pants");
+  const clickHandler = () => {
+    Alert.alert("Floating Button Clicked");
+    var newPostRef = pantsRef.push();
+    newPostRef.set({
+      cans: cansCount,
+      userId: user
+    });
+  };
+
   return (
-    <View>
+    <View style={styles.MainContainer}>
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={canAmount => setCanAmount(canAmount)}
+      />
+      <Text>Example of Floating Action Button</Text>
+      <Text>Click on Action Button to see Alert</Text>
+      <Text>{cansCount}</Text>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={clickHandler}
+        style={styles.TouchableOpacityStyle}
+      >
+        <Image
+          source={require("../assets/images/floating_button_green.png")}
+          style={styles.FloatingButtonStyle}
+        />
+      </TouchableOpacity>
       <Text>Min pant</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5F5F5"
+  },
+
+  TouchableOpacityStyle: {
+    position: "absolute",
+    width: 70,
+    height: 70,
+    alignItems: "center",
+    justifyContent: "center",
+    bottom: 30
+  },
+
+  FloatingButtonStyle: {
+    resizeMode: "contain",
+    width: 70,
+    height: 70
+  }
+});
