c9c1ea6db72483dba78bc0d394f78427b6421c4a
elisabethryan <ebethryan@gmail.com>
2020-02-19 13:50:51 +0100

    restored master

--- components/PantInfoPopUp.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export function PantInfoPopUp () {
-  return (
-    <Text>This is a different component now</Text>
-  );
-};
--- components/PantMap.js
+++ components/PantMap.js
@@ -6,7 +6,6 @@ import { EmailInput } from './EmailInput.js';
 export const PantMap = props => {
   const [modal, setModal] = useState(false)
   return (
-  <View>
     <MapView
       style={styles.map}
       initialRegion={{
@@ -17,43 +16,7 @@ export const PantMap = props => {
       }}
 
     >
-    <MapView.Marker
-        coordinate={{latitude: 59.8150,
-        longitude: 17.6629}}
-        title={"SLU"}
-        description={"example marker"}
-        >
-       <MapView.Callout tooltip style={styles.customView}>
-          <TouchableHighlight onPress= {()=>setModal(true)} underlayColor='#dddddd'>
-            <View style={styles.calloutText}>
-            <Text>{modal.toString()}
-            </Text>
-            </View>
-          </TouchableHighlight>
-        </MapView.Callout>
-    </MapView.Marker>
     </MapView>
-    <Modal
-          animationType="slide"
-          transparent={false}
-          visible={modal}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-          }}>
-          <View style={{marginTop: 22}}>
-            <View>
-              <Text>Hello World!</Text>
-
-              <TouchableHighlight
-                onPress={() => {
-                  this.setModalVisible(!modal);
-                }}>
-                <Text>Hide Modal</Text>
-              </TouchableHighlight>
-            </View>
-          </View>
-        </Modal>
-  </View>
   );
 };
 
