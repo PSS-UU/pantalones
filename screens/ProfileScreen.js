0d2899b2f96a4a9d74e6041d8aa3bb3f0477a7b1
Rebecka Nilsson <rebecka.nilsson.4667@student.uu.se>
2020-02-25 21:55:26 +0100

    added edit button, no functionality yet

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,17 +1,33 @@
 import React, { useEffect, useState } from "react";
 import firebase from "firebase";
-import { StyleSheet, Text, View, Button, Image, Alert, TextInput, TouchableHighlight,} from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  Alert,
+  TextInput,
+  TouchableHighlight
+} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { RectButton, ScrollView } from "react-native-gesture-handler";
-import * as ImagePicker from 'expo-image-picker';
+import {
+  RectButton,
+  ScrollView,
+  TouchableWithoutFeedback
+} from "react-native-gesture-handler";
+import * as ImagePicker from "expo-image-picker";
 
 export default function ProfileScreen() {
   const [imageUrl, setImageUrl] = useState();
   const user = firebase.auth().currentUser;
-  const profilePictureRef = firebase.storage().ref().child(`images/profiles/${user.uid}`);
+  const profilePictureRef = firebase
+    .storage()
+    .ref()
+    .child(`images/profiles/${user.uid}`);
   const infoRef = firebase.database().ref(`users/${user.uid}`);
-  const [nameInput, setNameInput] = useState({name: '', address: ''});
-  const [addressInput, setAddressInput] = useState({name: '', address: ''});
+  const [nameInput, setNameInput] = useState({ name: "", address: "" });
+  const [addressInput, setAddressInput] = useState({ name: "", address: "" });
   const [nameFrom, setNameFrom] = useState();
   const [addressFrom, setAddressFrom] = useState();
 
@@ -25,13 +41,13 @@ export default function ProfileScreen() {
 
   handleChange = e => {
     setNameInput({
-      name:e.nativeEvent.text
+      name: e.nativeEvent.text
     });
   };
 
   handleChangeAddress = e => {
     setAddressInput({
-      address:e.nativeEvent.text
+      address: e.nativeEvent.text
     });
   };
 
@@ -39,15 +55,15 @@ export default function ProfileScreen() {
     infoRef.set({
       name: nameInput,
       address: addressInput
-    })
-  }
+    });
+  };
 
   let addAddress = (nameInput, addressInput) => {
     infoRef.set({
       name: nameInput,
       address: addressInput
-    })
-  }
+    });
+  };
 
   handleSubmit = () => {
     addName(nameInput.name, addressInput.address);
@@ -75,101 +91,129 @@ export default function ProfileScreen() {
         Alert.alert("error", `Error: ${error}`);
       }
     }
-  }
+  };
 
   uploadImage = async uri => {
     const response = await fetch(uri);
     const blob = await response.blob();
-    
+
     await profilePictureRef.put(blob);
     const url = await profilePictureRef.getDownloadURL();
     setImageUrl(url);
-  }
+  };
 
   getName = () => {
-    infoRef.on("child_added", function(snapshot) {
-      console.log(snapshot.val());
-      setNameFrom(snapshot.val());
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
-    });
-  }
+    infoRef.on(
+      "child_added",
+      function(snapshot) {
+        console.log(snapshot.val());
+        setNameFrom(snapshot.val());
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      }
+    );
+  };
 
   getAddress = () => {
-    infoRef.once("child_added", function(snapshot) {
-      console.log(snapshot.val());
-      setAddressFrom(snapshot.val());
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
-    });
-  }
+    infoRef.once(
+      "child_added",
+      function(snapshot) {
+        console.log(snapshot.val());
+        setAddressFrom(snapshot.val());
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      }
+    );
+  };
 
   saveAndDisplay = () => {
     this.handleSubmit();
     this.getName();
-  } 
+  };
 
   saveAndDisplayAddress = () => {
     this.handleSubmitAddress();
     this.getAddress();
-  } 
+  };
 
   saveAndDisplayAll = () => {
     this.saveAndDisplay();
     this.saveAndDisplayAddress();
-  }
-  
+  };
 
   return (
     <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
+      style={profileStyles.container}
+      contentContainerStyle={profileStyles.contentContainer}
     >
-      <View
-        style={{justifyContent: 'center', alignItems: 'center',}}>
-        
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
         <Image
-          style={{height: 110, width: 110, borderRadius: 300/2, overflow: 'hidden'}}
-          source={{uri: imageUrl}}
+          resizeMode="cover"
+          style={profileStyles.profilePicture}
+          source={{ uri: imageUrl }}
         />
-        <TouchableHighlight style= {{paddingLeft: 100,}} onPress={this.onChooseImagePress}>
-          <Image
-            style={{height:30, width: 30}}
-            source={require('../assets/images/camera.png')}
-          />
-        </TouchableHighlight>  
-        <Text style={{fontSize: 20, paddingLeft: 20}}>
-          För- och efternamn:{" "} {nameFrom}
-        </Text>
 
-        <Text style={{fontSize: 20, paddingLeft: 20}}>
-        </Text>
-        <TextInput style={{ paddingLeft: 20, height: 30, width: 200, borderColor: 'gray', borderWidth: 1 }} onChange={handleChange} />
-        <Text style={{fontSize: 20, paddingLeft: 20}}>
-          Antal följare:
-        </Text>
+        <TouchableHighlight style={profileStyles.edit} size={25}>
+          <Image
+            style={{ height: 30, width: 30 }}
+            source={require("../assets/images/setting.png")}
+          />
+        </TouchableHighlight>
+
+        <TouchableHighlight
+          style={profileStyles.cameraButton}
+          onPress={this.onChooseImagePress}
+        >
+          <Image
+            style={{ height: 30, width: 30 }}
+            source={require("../assets/images/camera.png")}
+          />
+        </TouchableHighlight>
+
+        <View style={profileStyles.infoNameAndFollowers}>
+          <Text style={profileStyles.infoName}>För- och efternamn: {nameFrom}</Text>
+
+          <TextInput
+            style={{
+              paddingLeft: 20,
+              height: 30,
+              width: 200,
+              borderColor: "gray",
+              borderWidth: 1
+            }}
+            onChange={handleChange}
+          />
+          <Text style={{ fontSize: 20, paddingLeft: 20 }}>Antal följare:</Text>
+        </View>
       </View>
 
-
-      <View style={{paddingTop: 30}}>
-        
-        <Text style={{fontSize: 20, paddingLeft: 20}}>
+      <View style={{ paddingTop: 30 }}>
+        <Text style={{ fontSize: 20, paddingLeft: 20 }}>
           E-postadress:{" "}
-          <Text style={{ fontWeight: "bold" }}>{user ? user.email : "None"}</Text>
+          <Text style={{ fontWeight: "bold" }}>
+            {user ? user.email : "None"}
+          </Text>
         </Text>
 
-        <Text style={{fontSize: 20, paddingLeft: 20}}>
+        <Text style={{ fontSize: 20, paddingLeft: 20 }}>
           Adress: {addressFrom}
         </Text>
-        <TextInput style={{ paddingLeft: 20, height: 30, width: 200, borderColor: 'gray', borderWidth: 1 }} onChange={handleChangeAddress} />
-        
-        <Text style={{fontSize: 20, paddingLeft: 20}}>
-          Poäng:
-        </Text>
+        <TextInput
+          style={{
+            paddingLeft: 20,
+            height: 30,
+            width: 200,
+            borderColor: "gray",
+            borderWidth: 1
+          }}
+          onChange={handleChangeAddress}
+        />
 
-
+        <Text style={{ fontSize: 20, paddingLeft: 20 }}>Poäng:</Text>
       </View>
-      <Button title="Spara" onPress={saveAndDisplayAll}/>
+      <Button title="Spara" onPress={saveAndDisplayAll} />
       <Button title="Logga ut" onPress={logout} />
     </ScrollView>
   );
@@ -182,28 +226,31 @@ function OptionButton({ icon, label, onPress, isLastOption }) {
       onPress={onPress}
     >
       <View style={{ flexDirection: "row" }}>
-        <View style={styles.optionIconContainer}>
+        <View style={profileStyles.optionIconContainer}>
           <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
         </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
+        <View style={profileStyles.optionTextContainer}>
+          <Text style={profileStyles.optionText}>{label}</Text>
         </View>
       </View>
     </RectButton>
   );
 }
 
-const styles = StyleSheet.create({
+const profileStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fafafa"
   },
+
   contentContainer: {
     paddingTop: 15
   },
+
   optionIconContainer: {
-    marginRight: 12,
+    marginRight: 12
   },
+
   option: {
     backgroundColor: "#fdfdfd",
     paddingHorizontal: 15,
@@ -212,12 +259,45 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0,
     borderColor: "#ededed"
   },
+
   lastOption: {
     borderBottomWidth: StyleSheet.hairlineWidth
   },
+
   optionText: {
     fontSize: 15,
     alignSelf: "flex-start",
     marginTop: 1
-  }
+  },
+
+  profilePicture: {
+    flex: 1,
+    height: 115,
+    width: 115,
+    top: 5,
+    borderRadius: 300 / 2,
+    overflow: "hidden"
+  },
+
+  edit: {
+    margin: 5,
+    position: "absolute",
+    top: 10,
+    right: 35
+  },
+
+  cameraButton: {
+    position: "absolute",
+    top: 95,
+    right: 110
+  },
+
+  infoNameAndFollowers: {
+    top: 15
+  },
+
+  infoName: {
+    fontSize: 20
+  },
+
 });
