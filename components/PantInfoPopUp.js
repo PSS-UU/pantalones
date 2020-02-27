2f1f9e9628a21beb6143400fba3c122107a5dd9e
codemma <emma.tysk@gmail.com>
2020-02-27 11:49:35 +0100

    added styling and paxa-pant button

--- components/PantInfoPopUp.js
+++ components/PantInfoPopUp.js
@@ -6,11 +6,26 @@ import {
   TouchableHighlight,
   View,
   Text,
-  Image
+  Image,
+  TouchableOpacity,
+  Button,
+  ImageBackground
 } from "react-native";
 import StarRating from "react-native-star-rating";
+import Colors from "../constants/Colors";
+import globalStyles from "../AppStyles";
 
 export default function PantInfoPopUp({ pant, modal, setModal }) {
+  async function paxaPant() {
+    await ref.add({
+      cans: cansCount,
+      location: location,
+      userId: user
+    });
+    setCanAmount(0);
+    setModal(!modalVisible);
+  }
+
   return (
     <Modal
       transparent={true}
@@ -21,53 +36,62 @@ export default function PantInfoPopUp({ pant, modal, setModal }) {
       }}
     >
       <View style={styles.modalContent}>
-        <View style={styles.popup}>
-          <View style={styles.container}>
-            <Text style={styles.title}>PANT</Text>
-            <TouchableHighlight
-              style={styles.cross}
-              onPress={() => {
-                setModal(false);
-                console.log("modal:");
-                console.log(modal);
-              }}
+        <View style={styles.popupBackground}>
+          <Image
+            source={require("../assets/images/background-wave.png")}
+            style={styles.backgroundImage}
+          />
+          <View style={styles.modalContentContainer}>
+            <View style={styles.modalHeaderContainer}>
+              <Text style={styles.modalText}>Pant</Text>
+              <Button
+                style={styles.exitButton}
+                title="x"
+                onPress={() => setModal(false)}
+              />
+            </View>
+            <Text>Distance</Text>
+
+            <View style={styles.pantAmountContainer}>
+              <View style={styles.pantAmount}>
+                <Image
+                  style={styles.icon}
+                  source={require("../assets/images/can.png")}
+                />
+                <Text>Burkar</Text>
+                <Text>{pant.cans}</Text>
+              </View>
+              <View style={styles.pantAmountCenter}>
+                <Image
+                  style={styles.icon}
+                  source={require("../assets/images/can.png")}
+                />
+                <Text>Flaskor</Text>
+              </View>
+              <View style={styles.pantAmount}>
+                <Image
+                  style={styles.icon}
+                  source={require("../assets/images/can.png")}
+                />
+                <Text>Kronor</Text>
+              </View>
+            </View>
+            <View>
+              <Image
+                style={styles.icon}
+                source={require("../assets/images/can.png")}
+              />
+              <Text>Name</Text>
+            </View>
+            <StarRating></StarRating>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={paxaPant}
+              style={[globalStyles.lightGreenButton, styles.positionBottom]}
             >
-              <Text>X</Text>
-            </TouchableHighlight>
+              <Text style={globalStyles.buttonText}>Paxa pant!</Text>
+            </TouchableOpacity>
           </View>
-          <Text>Distance</Text>
-          <View style={styles.container}>
-            <View style={styles.pantAmount}>
-              <Image
-                style={styles.icon}
-                source={require("../assets/images/can.png")}
-              />
-              <Text>Burkar</Text>
-              <Text>{pant.cans}</Text>
-            </View>
-            <View style={styles.pantAmountCenter}>
-              <Image
-                style={styles.icon}
-                source={require("../assets/images/can.png")}
-              />
-              <Text>Flaskor</Text>
-            </View>
-            <View style={styles.pantAmount}>
-              <Image
-                style={styles.icon}
-                source={require("../assets/images/can.png")}
-              />
-              <Text>Kronor</Text>
-            </View>
-          </View>
-          <View>
-            <Image
-              style={styles.icon}
-              source={require("../assets/images/can.png")}
-            />
-            <Text>Name</Text>
-          </View>
-          <StarRating></StarRating>
         </View>
       </View>
     </Modal>
@@ -79,33 +103,58 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height
   },
-  container: {
+  modalContentContainer: {
+    alignItems: "flex-start",
+    flex: 1,
+    flexDirection: "column"
+  },
+  exitButton: {
+    alignSelf: "flex-end",
+    flex: 1
+  },
+  modalText: {
+    fontSize: 32,
+    color: Colors.mediumGreen,
+    alignSelf: "flex-start",
+    flex: 1,
+    fontFamily: "fredoka-one"
+  },
+
+  pantAmountContainer: {
+    flex: 1,
     flexDirection: "row",
-    alignItems: "flex-start"
+    padding: 20
   },
-  cross: {
-    alignItems: "flex-end",
-    flex: 1
-  },
-  title: {
-    flex: 1
-  },
+
   icon: {
     width: 32,
     height: 40
   },
-  popup: {
-    paddingLeft: "10%",
-    paddingRight: "10%",
-    paddingTop: "10%",
+  popupBackground: {
     borderRadius: 10,
     backgroundColor: "white",
     height: "90%",
     width: "90%"
   },
+  modalHeaderContainer: {
+    flexDirection: "row",
+    padding: 20
+  },
+  positionBottom: {
+    alignSelf: "flex-end"
+  },
   modalContent: {
     alignItems: "center",
-    margin: 0
+    margin: 0,
+    justifyContent: "center"
+  },
+  backgroundImage: {
+    width: "100%",
+    height: Dimensions.get("window").height / 3,
+    position: "absolute",
+    borderRadius: 10,
+    right: 0,
+    left: 0
   },
   pantAmountCenter: {
     alignItems: "center",
